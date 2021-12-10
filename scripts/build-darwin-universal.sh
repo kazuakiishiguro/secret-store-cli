cedb59e456bd38d955d3c075b9617861a85df88a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-12-11 07:58:57 +0900

    Update ci

--- scripts/build-darwin-universal.sh
+++ scripts/build-darwin-universal.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 TARGET_DIR="target/release"
 X64_TOOLCHAIN="nightly-x86_64-apple-darwin"
 CARGO_OUTPUT="$TARGET_DIR/secret-store-cli"
-X64_TARGET="$TARGET_DIR/secret-store_cli-x64.dylib"
+X64_TARGET="$TARGET_DIR/secret-store-cli-x64.dylib"
 FAT_TARGET="$TARGET_DIR/secret-store-cli.bundle"
 
 rustup run $X64_TOOLCHAIN cargo build --release && mv $CARGO_OUTPUT $X64_TARGET
