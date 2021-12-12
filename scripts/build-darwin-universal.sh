6e0d9467f3f93e07e45c109149f160677d25ab37
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-12-12 21:23:48 +0900

    fixed build script for osx

--- scripts/build-darwin-universal.sh
+++ scripts/build-darwin-universal.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 set -eu
-cd $(dirname $0)
 
-TARGET_DIR="target/release"
+CUR_DIR=$(pwd)
+TARGET_DIR="$CUR_DIR/target/release"
 X64_TOOLCHAIN="nightly-x86_64-apple-darwin"
 CARGO_OUTPUT="$TARGET_DIR/secret-store-cli"
 X64_TARGET="$TARGET_DIR/secret-store-cli-x64.dylib"
 FAT_TARGET="$TARGET_DIR/secret-store-cli.bundle"
 
-rustup run $X64_TOOLCHAIN cargo build --release && mv $CARGO_OUTPUT $X64_TARGET
-lipo $X64_TARGET -output $FAT_TARGET -create
+rustup run $X64_TOOLCHAIN cargo build --release && mv "$CARGO_OUTPUT" "$X64_TARGET"
+lipo "$X64_TARGET" -output "$FAT_TARGET" -create
